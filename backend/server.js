7f07347b07cae838d5d04f95a5f73502e0d272bc
Bikash Kumar Yadav <122146950+Kbikash24@users.noreply.github.com>
2024-05-17 22:08:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ mongoose
     console.error("Failed to connect to MongoDB", err);
   });
 app.get("/",(req,res)=>{res.json("hello")})
-const port = 5000;
+const port =  process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is working on port ${port}`);
