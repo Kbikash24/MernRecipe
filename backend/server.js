81ca4de3d3c5d79c8f01f83e47414b32faf6ea9e
Bikash Kumar Yadav <122146950+Kbikash24@users.noreply.github.com>
2024-05-17 22:29:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(cors({
-    origin:["https://mern-recipe-client.vercel.app"],
+    origin:["https://mern-recipe-client.vercel.app/api"],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true
 }))
